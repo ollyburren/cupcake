75e6278a0d892f64ff35329c6419d6816cd71ba3
ollyburren <oliver.burren@cimr.cam.ac.uk>
2017-12-14 10:08:45 +0000

    made wakefield_pp routine exportable

--- R/cupfunc.R
+++ R/cupfunc.R
@@ -29,6 +29,7 @@ logsum <- function(x) {
 #' value sets the variance of this distribution to 0.04, equivalent to a 95\%  belief that the true relative risk
 #' is in the range of 0.66-1.5 at any causal variant.
 #' @return a vector of posterior probabilities.
+#' @export
 
 wakefield_pp <- function(p,f, N, s,pi=1e-4,sd.prior=0.2) {
     if(length(p) != length(f))
