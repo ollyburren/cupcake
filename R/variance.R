94f76c7c3e0f9334a18e0c7d201b5d2fe9eae275
ollyburren <oliver.burren@cimr.cam.ac.uk>
2019-10-17 15:48:21 +0100

    Added sparse variance computation

--- R/variance.R
+++ R/variance.R
@@ -1,5 +1,60 @@
 library(snpStats)
 
+
+#' An internal helper function to get obtain standard error of beta estimates from a GWAS.DT data.frame object
+#' \code{get_seb}
+#' @param gwas.DT a data.table - variants and summary statistics
+#' @return a data.table with seb column added if not already present
+
+get_seb <- function(gwas.DT){
+  if(!any(names(gwas.DT)=='seb')){
+    if(any(names(gwas.DT)=='or')){
+      gwas.DT[,seb:=abs(log(or)/qnorm(p.value/2,lower.tail=FALSE))]
+    }else if(any(names(gwas.DT)=='beta')){
+      gwas.DT[,seb:=abs(beta/qnorm(p.value/2,lower.tail=FALSE))]
+    }else{
+      message("Cannot find required summary statistics aborting")
+      return()
+    }
+  }
+  gwas.DT[!is.finite(seb),seb:=0]
+}
+
+#' analytically compute the variance of a projection given a standard error of beta for a given trait for sparse basis
+#' \code{compute_proj_var}
+#'
+#' @param gwas.DT a data.table - variants and summary statistics
+#' @param shrink.DT a data.table - as returned by \code{\link{compute_shrinkage_metrics}}
+#' @param w.DT a data.table - a data table of weights/rotations from a  basis first column is 'pid' and subsequent columns for each principal component.
+#' @param sm snpMatrix object - a snp matrix object of reference genotypes for
+#' @param method scalar - shrinkage method to use (default shrinkage) but also valid is none
+#' @param quiet a scalar - boolean whether to show progress messages
+#' @return a scalar of variances for each principal component
+#' @export
+
+compute_seb_proj_var_sparse <- function(gwas.DT,shrink.DT,w.DT,sm,method='shrinkage',quiet=FALSE){
+  if(method=='none')
+    message("Warning: Method 'none' selected no weighting applied")
+  gwas.DT <- merge(gwas.DT,shrink.DT[,.(pid,shrinkage=get(`method`))],by='pid')
+  gwas.DT <- get_seb(gwas.DT)
+  gwas.DT[,c('chr','pos'):=tstrsplit(pid,':')]
+  gwas.DT <- merge(gwas.DT,w.DT,by='pid')
+  pids <- colnames(sm)
+  sm.map <- match(gwas.DT$pid,pids)
+  if(any(is.na(sm.map))){
+    message("SNPs in manifest that don't have genotypes")
+  }
+  r <- ld(sm[,sm.map],sm[,sm.map],stats="R")
+  if(any(is.na(r)))
+    if(!quiet)
+      message(sprintf("Found %s where R^2 is NA",sum(is.na(r))))
+  r[is.na(r)]<-0
+  pc.cols <- which(grepl("^PC[0-9]+$",names(gwas.DT)))
+  sapply(names(gwas.DT)[pc.cols],function(pc) (tcrossprod(gwas.DT[,.(tot=get(`pc`) * shrinkage * seb)]$tot) * r) %>% sum)
+}
+
+
+
 #' analytically compute the variance of a projection given a standard error of beta for a given trait
 #' \code{compute_proj_var}
 #'
@@ -21,17 +76,18 @@ compute_seb_proj_var <- function(gwas.DT,shrink.DT,man.DT,w.DT,ref_gt_dir,method
   gwas.DT <- merge(gwas.DT,shrink.DT[,.(pid,shrinkage=get(`method`))],by='pid')
   ## where possible compute se_beta
   ## sometimes our input will be for a quantitative trait
-  if(!any(names(gwas.DT)=='seb')){
-    if(any(names(gwas.DT)=='or')){
-      gwas.DT[,seb:=abs(log(or)/qnorm(p.value/2,lower.tail=FALSE))]
-    }else if(any(names(gwas.DT)=='beta')){
-      gwas.DT[,seb:=abs(beta/qnorm(p.value/2,lower.tail=FALSE))]
-    }else{
-      message("Cannot find required summary statistics aborting")
-      return()
-    }
-  }
-  gwas.DT[!is.finite(seb),seb:=0]
+  # if(!any(names(gwas.DT)=='seb')){
+  #   if(any(names(gwas.DT)=='or')){
+  #     gwas.DT[,seb:=abs(log(or)/qnorm(p.value/2,lower.tail=FALSE))]
+  #   }else if(any(names(gwas.DT)=='beta')){
+  #     gwas.DT[,seb:=abs(beta/qnorm(p.value/2,lower.tail=FALSE))]
+  #   }else{
+  #     message("Cannot find required summary statistics aborting")
+  #     return()
+  #   }
+  # }
+  # gwas.DT[!is.finite(seb),seb:=0]
+  gwas.DT <- get_seb(gwas.DT)
   gwas.DT[,c('chr','pos'):=tstrsplit(pid,':')]
   ## next add ld.block information
   gwas.DT <- merge(gwas.DT,man.DT[,.(pid,ld.block)],by='pid')
