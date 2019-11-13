7143ef65f9d9099f0e449eadf91070ab9affcb1d
ollyburren <oliver.burren@cimr.cam.ac.uk>
2019-11-13 09:24:50 +0000

    Added sparse projection vignette

---
+++ R/burren_imd_13_traits.R
@@ -0,0 +1,151 @@
+#' Burren sparse basis files
+#'
+#' Burren sparse basis files
+#'
+#' @docType data
+#'
+#' @usage data(burren_imd_13_traits)
+#'
+#' @format TODO
+#'
+#' @keywords datasets
+#'
+#' @references TODO
+#'
+#'
+#' @examples
+#' data(burren_imd_13_traits)
+"rot.pca"
+
+#' Burren sparse basis files
+#'
+#' Burren sparse basis files
+#'
+#' @docType data
+#'
+#' @usage data(burren_imd_13_traits)
+#'
+#' @format TODO
+#'
+#' @keywords datasets
+#'
+#' @references TODO
+#'
+#'
+#' @examples
+#' data(burren_imd_13_traits)
+"use.pca"
+
+#' Burren sparse basis files
+#'
+#' Burren sparse basis files
+#'
+#' @docType data
+#'
+#' @usage data(burren_imd_13_traits)
+#'
+#' @format TODO
+#'
+#' @keywords datasets
+#'
+#' @references TODO
+#'
+#'
+#' @examples
+#' data(burren_imd_13_traits)
+"beta.centers"
+
+#' Burren sparse basis files
+#'
+#' Burren sparse basis files
+#'
+#' @docType data
+#'
+#' @usage data(burren_imd_13_traits)
+#'
+#' @format TODO
+#'
+#' @keywords datasets
+#'
+#' @references TODO
+#'
+#'
+#' @examples
+#' data(burren_imd_13_traits)
+"shrinkage"
+
+#' Burren sparse basis files
+#'
+#' Burren sparse basis files
+#'
+#' @docType data
+#'
+#' @usage data(burren_imd_13_traits)
+#'
+#' @format TODO
+#'
+#' @keywords datasets
+#'
+#' @references TODO
+#'
+#'
+#' @examples
+#' data(burren_imd_13_traits)
+"LD"
+
+#' Burren sparse basis files
+#'
+#' Burren sparse basis files
+#'
+#' @docType data
+#'
+#' @usage data(burren_imd_13_traits)
+#'
+#' @format TODO
+#'
+#' @keywords datasets
+#'
+#' @references TODO
+#'
+#'
+#' @examples
+#' data(burren_imd_13_traits)
+"SNP.manifest"
+
+#' Burren sparse basis files
+#'
+#' Burren sparse basis files
+#'
+#' @docType data
+#'
+#' @usage data(burren_imd_13_traits)
+#'
+#' @format TODO
+#'
+#' @keywords datasets
+#'
+#' @references TODO
+#'
+#'
+#' @examples
+#' data(burren_imd_13_traits)
+"project.sparse"
+
+#' Burren sparse basis files
+#'
+#' Burren sparse basis files
+#'
+#' @docType data
+#'
+#' @usage data(burren_imd_13_traits)
+#'
+#' @format TODO
+#'
+#' @keywords datasets
+#'
+#' @references TODO
+#'
+#'
+#' @examples
+#' data(burren_imd_13_traits)
+"stats"
---
+++ R/lyons_egpa.R
@@ -0,0 +1,20 @@
+#' EGPA combined GWAS summary statistics
+#'
+#' Summary data from GWAS study of EGPA of 534 cases and 6,688 controls
+#'
+#' @docType data
+#'
+#' @usage data(lyons_egpa)
+#'
+#' @format A data.table object of odds ratios, beta standard errors and p values.
+#'
+#' @keywords datasets
+#'
+#' @references Lyons et al. Nat Commun 10, 5120 (2019) doi:10.1038/s41467-019-12515-9
+#' (\href{https://doi.org/10.1038/s41467-019-12515-9}{DOI})
+#'
+#'
+#' @examples
+#' data(lyons_egpa)
+#' head(lyons_egpa)
+"lyons_egpa"
